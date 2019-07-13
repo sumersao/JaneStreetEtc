ab2d795b21a46cd05b661f61ca35321efdd8df8a
sumersao <saosumer@gmail.com>
2019-07-13 14:37:31 -0700

    a

--- prelim.cpp
+++ prelim.cpp
@@ -222,6 +222,7 @@ int main(int argc, char *argv[])
   //   sell.push_back(string("1001"));
   //   sell.push_back(string("99"));
   //   conn.send_to_exchange(join(" ", sell));
+  int bookreads = 0;
   while(1) {
       //read from the exchange
     string curline = conn.read_from_exchange();
@@ -239,6 +240,7 @@ int main(int argc, char *argv[])
       cout << endl;
 
     if(curline.find("BOOK") == 0) {
+      bookreads++;
         //type is res[1]"
         //let's go through buy and buy all of the values less than 100
 
@@ -288,8 +290,9 @@ int main(int argc, char *argv[])
         // SETS INTIAL VALUE
         fair_value_map[res[1]] = (temp2 + temp1)/2.0;
       } else {
-        double smoothing = 2.0 / (10.0 + 1.0);
-        fair_value_map[res[1]] = (temp1 + temp2)/2.0 * (smoothing/(1.0 + 10.0)) + fair_value_map[res[1]] * (1 - smoothing/(9.0));
+        double val = min(bookreads, 10);
+        double smoothing = 2.0 / (val + 1.0);
+        fair_value_map[res[1]] = (temp1 + temp2)/2.0 * (smoothing/(1.0 + val)) + fair_value_map[res[1]] * (1 - smoothing/(val + 1.0));
       }
 
 
