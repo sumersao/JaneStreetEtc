f3394b75c9a91c171598c10a2f665b70402ea697
sumersao <saosumer@gmail.com>
2019-07-13 19:40:54 -0700

    a

--- prelim.cpp
+++ prelim.cpp
@@ -235,7 +235,7 @@ int main(int argc, char *argv[])
   cout << "The exchange replied: " << line << endl;
 
   int ids = 1;
-  int Nsize = 10;
+  int Nsize = 5;
   double smoother = 2.0/(1.0 + 1.0*Nsize);
     //start our trading here
   while(1) {
@@ -347,7 +347,7 @@ int main(int argc, char *argv[])
 
       // cout << lastFV[curind] << " " << fairval << endl;
 
-      if(abs(fairval - lastFV[curind]) >= 2) {
+      if(abs(fairval - lastFV[curind]) >= 3) {
         //cancel our last two orders
         conn.send_to_exchange("CANCEL " + to_string(lastids[curind].first));
         conn.send_to_exchange("CANCEL " + to_string(lastids[curind].second));
@@ -360,7 +360,7 @@ int main(int argc, char *argv[])
         buy.push_back(to_string(ids+1));
         buy.push_back(res[1]);
         buy.push_back(string("BUY"));
-        buy.push_back(to_string(int(fairval - 5)));
+        buy.push_back(to_string(int(fairval - 7)));
         buy.push_back(to_string(3));
         conn.send_to_exchange(join(" ", buy));
 
@@ -374,7 +374,7 @@ int main(int argc, char *argv[])
         sell.push_back(to_string(ids+2));
         sell.push_back(res[1]);
         sell.push_back(string("SELL"));
-        sell.push_back(to_string(int(fairval + 5)));
+        sell.push_back(to_string(int(fairval + 7)));
         sell.push_back(to_string(3));
         conn.send_to_exchange(join(" ", sell));
 
